cdb652b21b5bd115d07ce69b257ba9d104f6a46d
Derek Fountain <derek@scratters.com>
2023-03-02 09:24:43 +0000

    first build of the IO firmware. contains 'blink'. works though

---
+++ firmware-io/zx_pico_if1_io_fw.c
@@ -0,0 +1,304 @@
+/*
+ * ZX Pico IF1 Firmware, a Raspberry Pi Pico based ZX Interface One emulator
+ * Copyright (C) 2023 Derek Fountain
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+#include "hardware/timer.h"
+
+
+/* 1 instruction on the 133MHz microprocessor is 7.5ns */
+/* 1 instruction on the 140MHz microprocessor is 7.1ns */
+/* 1 instruction on the 150MHz microprocessor is 6.6ns */
+/* 1 instruction on the 200MHz microprocessor is 5.0ns */
+
+//#define OVERCLOCK 150000
+//#define OVERCLOCK 200000
+
+const uint8_t LED_PIN = PICO_DEFAULT_LED_PIN;
+
+/*
+ * These pin values are the GPxx ones in green background on the pinout diagram.
+ * See schematic for how the signals are fed into the Pico's GPIOs
+ */
+const uint8_t A0_GP          = 11;
+const uint8_t A1_GP          = 12;
+const uint8_t A2_GP          = 13;
+const uint8_t A3_GP          = 14;
+const uint8_t A4_GP          = 20;
+const uint8_t A5_GP          = 21;
+const uint8_t A6_GP          = 22;
+const uint8_t A7_GP          = 26;
+
+/*
+ * Given the GPIOs with an address bus value on them, this packs the
+ * 14 address bits down into the least significant 14 bits
+ */
+inline uint16_t pack_address_gpios( uint32_t gpios )
+{
+  /*     Bits 0,1,2,3,4,5       Bits 6,7,8,9,10,11,12     Bit 13                   */
+  return ((gpios>>9) & 0x03F) | ((gpios>>10) & 0x1FC0) | ((gpios & 0x4000000) >> 13);
+}
+
+/*
+ * Given value i, this calculates the pattern of the GPIOs if
+ * value i were to appear on the Z80 address bus.
+ */
+uint32_t create_gpios_for_address( uint32_t i )
+{
+  uint32_t gpio_pattern = 
+    ( ((i & 0x0001) >>  0) <<  A0_GP ) |
+    ( ((i & 0x0002) >>  1) <<  A1_GP ) |
+    ( ((i & 0x0004) >>  2) <<  A2_GP ) |
+    ( ((i & 0x0008) >>  3) <<  A3_GP ) |
+    ( ((i & 0x0010) >>  4) <<  A4_GP ) |
+    ( ((i & 0x0020) >>  5) <<  A5_GP ) |
+    ( ((i & 0x0040) >>  6) <<  A6_GP ) |
+    ( ((i & 0x0080) >>  7) <<  A7_GP );
+
+  return gpio_pattern;
+}
+
+
+const uint8_t  D0_GP          = 0;
+const uint8_t  D1_GP          = 1;
+const uint8_t  D2_GP          = 2;
+const uint8_t  D3_GP          = 4; 
+const uint8_t  D4_GP          = 6;
+const uint8_t  D5_GP          = 3;
+const uint8_t  D6_GP          = 5;
+const uint8_t  D7_GP          = 7;
+
+const uint32_t  D0_BIT_MASK  = ((uint32_t)1 <<  D0_GP);
+const uint32_t  D1_BIT_MASK  = ((uint32_t)1 <<  D1_GP);
+const uint32_t  D2_BIT_MASK  = ((uint32_t)1 <<  D2_GP);
+const uint32_t  D3_BIT_MASK  = ((uint32_t)1 <<  D3_GP);
+const uint32_t  D4_BIT_MASK  = ((uint32_t)1 <<  D4_GP);
+const uint32_t  D5_BIT_MASK  = ((uint32_t)1 <<  D5_GP);
+const uint32_t  D6_BIT_MASK  = ((uint32_t)1 <<  D6_GP);
+const uint32_t  D7_BIT_MASK  = ((uint32_t)1 <<  D7_GP);
+
+const uint8_t  ROM_READ_GP              = 8;
+const uint32_t ROM_READ_BIT_MASK        = ((uint32_t)1 << ROM_READ_GP);
+
+/* Z80's M1 signal, needs to be merged into the IF1 paging logic */
+const uint8_t  M1_GP                    = 15;
+const uint32_t M1_INPUT_BIT_MASK        = ((uint32_t)1 << M1_GP);
+
+/*
+ * Data bus leve shifter direction pin, 1 is zx->pico, which is the normal
+ * position, 0 means pico->zx which is temporarily switched to when the
+ * Pico wants to send a data byte back to the Spectrum
+ */
+const uint8_t  DIR_OUTPUT_GP            = 27;
+
+/* This pin triggers a transistor which shorts the Z80's /RESET to ground */
+const uint8_t  PICO_RESET_Z80_GP        = 28;
+
+const uint32_t DBUS_MASK     = ((uint32_t)1 << D0_GP) |
+                               ((uint32_t)1 << D1_GP) |
+                               ((uint32_t)1 << D2_GP) |
+                               ((uint32_t)1 << D3_GP) |
+                               ((uint32_t)1 << D4_GP) |
+                               ((uint32_t)1 << D5_GP) |
+                               ((uint32_t)1 << D6_GP) |
+                               ((uint32_t)1 << D7_GP);
+
+
+
+
+
+int main()
+{
+  bi_decl(bi_program_description("ZX Spectrum Pico IF1 board binary."));
+
+#ifdef OVERCLOCK
+  set_sys_clock_khz( OVERCLOCK, 1 );
+#endif
+
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  while( 1 )
+  {
+    gpio_put(LED_PIN, 1);
+    busy_wait_us_32(250000);
+    gpio_put(LED_PIN, 0);
+    busy_wait_us_32(250000);
+  }
+
+}
+
+
+#if 0
+
+  /*
+   * Set up Pico's Z80 reset pin, hold this at 0 to let Z80 run.
+   * Set and hold 1 here to hold Spectrum in reset at startup until we're good
+   * to provide its ROM
+   */
+  gpio_init( PICO_RESET_Z80_GP );  gpio_set_dir( PICO_RESET_Z80_GP, GPIO_OUT );
+  gpio_put( PICO_RESET_Z80_GP, 1 );
+
+  /* All interrupts off except the timers */
+  irq_set_mask_enabled( 0xFFFFFFFF, 0 );
+  irq_set_mask_enabled( 0x0000000F, 1 );
+
+  /* Create address indirection table, this is the address bus optimisation  */
+  create_indirection_table();
+
+  /* Switch the bits in the ROM bytes around, this is the data bus optimisation */
+  preconvert_roms();
+
+  /* Pull the buses to zeroes */
+  gpio_init( A0_GP  ); gpio_set_dir( A0_GP,  GPIO_IN );  gpio_pull_down( A0_GP  );
+  gpio_init( A1_GP  ); gpio_set_dir( A1_GP,  GPIO_IN );  gpio_pull_down( A1_GP  );
+  gpio_init( A2_GP  ); gpio_set_dir( A2_GP,  GPIO_IN );  gpio_pull_down( A2_GP  );
+  gpio_init( A3_GP  ); gpio_set_dir( A3_GP,  GPIO_IN );  gpio_pull_down( A3_GP  );
+  gpio_init( A4_GP  ); gpio_set_dir( A4_GP,  GPIO_IN );  gpio_pull_down( A4_GP  );
+  gpio_init( A5_GP  ); gpio_set_dir( A5_GP,  GPIO_IN );  gpio_pull_down( A5_GP  );
+  gpio_init( A6_GP  ); gpio_set_dir( A6_GP,  GPIO_IN );  gpio_pull_down( A6_GP  );
+  gpio_init( A7_GP  ); gpio_set_dir( A7_GP,  GPIO_IN );  gpio_pull_down( A7_GP  );
+  gpio_init( A8_GP  ); gpio_set_dir( A8_GP,  GPIO_IN );  gpio_pull_down( A8_GP  );
+  gpio_init( A9_GP  ); gpio_set_dir( A9_GP,  GPIO_IN );  gpio_pull_down( A9_GP  );
+  gpio_init( A10_GP ); gpio_set_dir( A10_GP, GPIO_IN );  gpio_pull_down( A10_GP );
+  gpio_init( A11_GP ); gpio_set_dir( A11_GP, GPIO_IN );  gpio_pull_down( A11_GP );
+  gpio_init( A12_GP ); gpio_set_dir( A12_GP, GPIO_IN );  gpio_pull_down( A12_GP );
+  gpio_init( A13_GP ); gpio_set_dir( A13_GP, GPIO_IN );  gpio_pull_down( A13_GP );
+
+  gpio_init( D0_GP  ); gpio_set_dir( D0_GP,  GPIO_IN );
+  gpio_init( D1_GP  ); gpio_set_dir( D1_GP,  GPIO_IN );
+  gpio_init( D2_GP  ); gpio_set_dir( D2_GP,  GPIO_IN );
+  gpio_init( D3_GP  ); gpio_set_dir( D3_GP,  GPIO_IN );
+  gpio_init( D4_GP  ); gpio_set_dir( D4_GP,  GPIO_IN );
+  gpio_init( D5_GP  ); gpio_set_dir( D5_GP,  GPIO_IN );
+  gpio_init( D6_GP  ); gpio_set_dir( D6_GP,  GPIO_IN );
+  gpio_init( D7_GP  ); gpio_set_dir( D7_GP,  GPIO_IN );
+
+  /* Init the direction pin */
+  gpio_init(DIR_OUTPUT_GP); gpio_set_dir(DIR_OUTPUT_GP, GPIO_OUT);
+  gpio_put(DIR_OUTPUT_GP, 1);
+
+  /* Input from logic hardware, indicates the ROM is being read by the Z80 */
+  gpio_init( ROM_READ_GP ); gpio_set_dir( ROM_READ_GP, GPIO_IN );
+  gpio_pull_down( ROM_READ_GP );
+
+  /* M1 signal input */
+  gpio_init( M1_GP ); gpio_set_dir( M1_GP, GPIO_IN );
+  gpio_pull_up( M1_GP );
+
+  /* Blip LED to show we're running */
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+  int signal;
+  for( signal=0; signal<2; signal++ )
+  {
+    gpio_put(LED_PIN, 1);
+    busy_wait_us_32(250000);
+    gpio_put(LED_PIN, 0);
+    busy_wait_us_32(250000);
+  }
+  gpio_put(LED_PIN, 1);
+
+  /*
+   * Ready to go, give it a few milliseconds for this Pico code to get into
+   * its main loop, then let the Z80 start
+   */
+  add_alarm_in_ms( 5, start_z80_alarm_func, NULL, 0 );
+
+
+  gpio_set_dir_in_masked( DBUS_MASK );
+  while(1)
+  {
+    register uint32_t gpios_state;
+
+    /*
+     * Spin while the hardware is saying at least one of A14, A15 and MREQ is 1.
+     * ROM_READ is active low - if it's 1 then the ROM is not being read.
+     */
+    while( (gpios_state=gpio_get_all()) & ROM_READ_BIT_MASK );
+
+    register uint16_t raw_bit_pattern = pack_address_gpios( gpios_state );
+
+    register uint16_t rom_address = address_indirection_table[raw_bit_pattern];
+
+    register uint8_t rom_value = *(rom_image_ptr+rom_address);
+
+    /*
+     * The level shifter for the data bus lines is switched to Pico->ZX,
+     * the Pico GPIOs are set to outputs and the value asserted for the
+     * Z80 to collect
+     */
+    gpio_put(DIR_OUTPUT_GP, 0);
+    gpio_set_dir_out_masked( DBUS_MASK );
+    gpio_put_masked( DBUS_MASK, rom_value );
+
+    /*
+     * Spin until the Z80 releases MREQ indicating the read is complete.
+     * ROM_READ is active low - if it's 0 then the ROM is still being read.
+     */
+    while( (gpio_get_all() & ROM_READ_BIT_MASK) == 0 );
+
+    /*
+     * Set the data bus GPIOs back to inputs, then switch the level shifter
+     * for the data bus lines back to ZX->Pico
+     */
+    gpio_set_dir_in_masked( DBUS_MASK );
+    gpio_put(DIR_OUTPUT_GP, 1);
+
+    /*
+     * M1 active means that that was an instruction fetch the Z80 just did.
+     * If it's one of the magic addresses, page the IF1 ROM
+     */
+    if( (gpios_state & M1_INPUT_BIT_MASK) == 0 )
+    {
+      if( (rom_address == 0x0008) || (rom_address == 0x1708) )
+      {
+	gpio_put(LED_PIN, 1);
+	rom_image_ptr = __ROMs_if1_rom;
+      }
+      else if( rom_address == 0x0700 )
+      {
+	rom_image_ptr = __ROMs_48_original_rom;
+	gpio_put(LED_PIN, 0);
+      }
+    }
+
+
+  } /* Infinite loop */
+
+}
+
+
+
+#if 0
+/* Blip the result pin, shows on scope */
+gpio_put( TEST_OUTPUT_GP, 1 ); busy_wait_us_32(5);
+gpio_put( TEST_OUTPUT_GP, 0 ); busy_wait_us_32(5);
+
+gpio_put( TEST_OUTPUT_GP, 1 ); busy_wait_us_32(1000);
+gpio_put( TEST_OUTPUT_GP, 0 ); busy_wait_us_32(1000);
+__asm volatile ("nop");
+#endif
+
+#endif
